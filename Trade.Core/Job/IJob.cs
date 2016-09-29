a5d54ad731b7eb1606ffb7cb768ed55a457a0544
Yuki Toida <yuki.toida.dev@gmail.com>
2016-09-30 00:13:48 +0900

    initial commit

---
+++ Trade.Core/Job/IJob.cs
@@ -0,0 +1,14 @@
+﻿namespace Trade.Core.Job
+{
+    public interface IJob
+    {
+        JobResultCode Execute(string[] args);
+    }
+
+    public enum JobResultCode
+    {
+        Success = 0,
+        Fail = 1,
+        Timeout = 2,
+    }
+}
