5ddfc43f6ad93452c09acd463cc85d01f78f9d04
戸井田 裕貴 <y.toida@gloops.com>
2017-03-06 15:04:37 +0900

    add webjobs to UI.Web

--- Trade.UI.Console/publish.bat
+++
@@ -1,4 +0,0 @@
-SET PublishPath=%CD%\WebJobs\Scraping
-dotnet build
-dotnet publish --output %PublishPath%
-powershell Compress-Archive -Path %PublishPath% -DestinationPath %PublishPath%.zip -Update
